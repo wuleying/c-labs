574f5fbae1583b87bc656462135c9ac28a09f75d
罗亮 <luoliang1@staff.weibo.com>
2017-05-06 10:11:17 +0800

    修改cmake脚本

--- luodb_v1/shell/cmake.sh
+++ luodb_v1/shell/cmake.sh
@@ -2,31 +2,31 @@
 
 project_name="luodb"
 
-root_dir=$(cd "$(dirname "$1")"; pwd)
+root_dir=$(cd "$(dirname "$1")" || exit; pwd)
 echo "root_dir:     $root_dir"
 
-build_dir=$root_dir/build
+build_dir="$root_dir"/build
 echo "build_dir:    $build_dir"
 
-bin_dir=$root_dir/bin
+bin_dir="$root_dir"/bin
 echo "bin_dir:      $bin_dir"
 
 echo "cleanup start..."
 
-if [[ -d $bin_dir ]]; then
-    find $bin_dir/* | xargs rm
+if [[ -d "$bin_dir" ]]; then
+    find "$bin_dir"/* -exec rm {} \;
 else
-    mkdir $bin_dir
+    mkdir "$bin_dir"
 fi
 
-if [[ -d $build_dir ]]; then
-    find $build_dir/* | xargs rm -rf
+if [[ -d "$build_dir" ]]; then
+    find "$build_dir"/* -exec rm -rf {} \;
 else
-    mkdir $build_dir
+    mkdir "$build_dir"
 fi
 
 echo "cd $build_dir"
-cd $build_dir
+cd "$build_dir" || exit
 
 echo "cmake .."
 cmake ..
@@ -34,9 +34,9 @@ cmake ..
 echo "make"
 make
 
-if [[ -f $bin_dir/$project_name ]]; then
-    strip $bin_dir/$project_name
-    chmod +x $bin_dir/$project_name
+if [[ -f "$bin_dir"/"$project_name" ]]; then
+    strip "$bin_dir"/"$project_name"
+    chmod +x "$bin_dir"/"$project_name"
 fi
 
 echo "done."
