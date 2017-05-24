20f9ae56d53fa0465131553b98fd4bff45d2ef19
罗亮 <luoliang1@staff.weibo.com>
2017-05-24 14:57:29 +0800

    update

--- luodb_v1/luodb/event/event.h
+++ luodb_v1/luodb/event/event.h
@@ -11,4 +11,56 @@
 #ifndef LUODB_EVENT_H
 #define LUODB_EVENT_H
 
+struct luo_event_loop_s;
+
+typedef void luo_file_proc_t(struct luo_event_loop_s *event_loop, int fd, void *client_data, int mask);
+
+typedef int luo_time_proc_t(struct luo_event_loop_s *event_loop, long long id, void *client_data);
+
+typedef void luo_event_finalizer_proc_t(struct luo_event_loop_s *event_loop, void *client_data);
+
+typedef struct luo_file_event_s {
+    int                        fd;
+    int                        mask;
+    luo_file_proc_t            *file_proc;
+    luo_event_finalizer_proc_t *finalizer_proc;
+    void                       *client_data;
+    struct luo_file_event_s    *next;
+
+} luo_file_event_t;
+
+typedef struct luo_time_event_s {
+    long long                  id;
+    long                       when_sec;
+    long                       when_ms;
+    luo_time_proc_t            *time_proc;
+    luo_event_finalizer_proc_t *finalizer_proc;
+    void                       *client_data;
+    struct luo_time_event_s    *next;
+
+} luo_time_event_t;
+
+typedef struct luo_event_loop_s {
+    long long        time_event_next_id;
+    luo_file_event_t *file_event_head;
+    luo_time_event_t *time_event_head;
+    int              stop;
+
+} luo_event_loop_t;
+
+/* 常量 */
+#define LUO_EVENT_OK            0
+#define LUO_EVENT_ERR           1
+
+#define LUO_EVENT_READABLE      1
+#define LUO_EVENT_WRITABLE      2
+#define LUO_EVENT_EXCEPTION     4
+
+#define LUO_EVENT_FILE_EVENTS   1
+#define LUO_EVENT_TIME_EVENTS   2
+#define LUO_EVENT_ALL_EVENTS    (LUO_EVENT_FILE_EVENTS | LUO_EVENT_TIME_EVENTS)
+#define LUO_EVENT_DONE_WAIT     4
+
+#define LUO_EVENT_NOMORE        -1
+
 #endif //LUODB_EVENT_H
