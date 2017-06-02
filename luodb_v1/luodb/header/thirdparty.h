2ce7cb4e46b8bf04fd08ce4deb5bf4db58208fe2
罗亮 <luoliang1@staff.weibo.com>
2017-06-02 14:31:02 +0800

    update lzf

---
+++ luodb_v1/luodb/header/thirdparty.h
@@ -0,0 +1,11 @@
+//
+// Created by luoliang1 on 2017/6/2.
+//
+
+#ifndef LUODB_HEADER_THIRDPARTY_H
+#define LUODB_HEADER_THIRDPARTY_H
+
+#include <luodb/thirdparty/lzf/lzf.h>
+#include <luodb/thirdparty/lzf/lzfP.h>
+
+#endif //LUODB_HEADER_THIRDPARTY_H
