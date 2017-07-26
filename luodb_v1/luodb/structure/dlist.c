3fd7e71ef6fa1ebfdeb698e225fa4a31f9f23f95
wuleying <5590548@qq.com>
2017-07-26 15:01:21 +0800

    update

--- luodb_v1/luodb/structure/dlist.c
+++ luodb_v1/luodb/structure/dlist.c
@@ -147,155 +147,201 @@ luoDListAddNodeTail(luo_dlist *dlist, void *value) {
 void
 luoDListDeleteNode(luo_dlist *dlist, luo_dlist_node *node) {
     if (node->prev) {
+        // 如果存在前驱节点,将前驱节点的后继修改为当前节点的后继节点
         node->prev->next = node->next;
     } else {
+        // 如果不存在前驱节点,说明当前节点为头节点,将链表的头节点修改为当前节点的后继节点
         dlist->head = node->next;
     }
 
     if (node->next) {
+        // 如果存在后继节点,将后继节点的前驱修改为当前节点的前驱节点
         node->next->prev = node->prev;
     } else {
+        // 如果不存在后继节点,说明当前节点为尾节点,将链表的尾节点修改为当前节点的前驱节点
         dlist->tail = node->prev;
     }
 
     if (dlist->free) {
+        // 如果定义了内存释放函数,释放当前节点值
         dlist->free(node->value);
     }
 
+    // 释放节点
     luoFree(node);
 
+    // 链表长度减一
     dlist->len--;
 }
 
+// 获取链表迭代器
 luo_dlist_iter *
 luoDListGetIterator(luo_dlist *dlist, int direction) {
+    // 为迭代器申请内存空间
     luo_dlist_iter *iter = luoMalloc(sizeof(luo_dlist_iter));
 
+    // 如果申请内存失败直接返回NULL
     if (iter == NULL) {
         return NULL;
     }
 
+    // 根据迭代器迭代方向设置从头节点还是尾节点开始遍历
     iter->next = (direction == LUO_DLIST_START_HEAD) ? dlist->head : dlist->tail;
 
+    // 设置迭代器迭代方向
     iter->direction = direction;
 
     return iter;
 }
 
+// 释放迭代器内存
 void
 luoDListReleaseIterator(luo_dlist_iter *iter) {
+    // 直接释放
     luoFree(iter);
 }
 
+// 根据迭代器获取下一节点
 luo_dlist_node *
 luoDListNext(luo_dlist_iter *iter) {
+    // 设置当前节点
     luo_dlist_node *current = iter->next;
 
     if (current != NULL) {
+        // 根据迭代器方向,设置迭代器下一个要处理的节点
         iter->next = (iter->direction == LUO_DLIST_START_HEAD) ? current->next : current->prev;
     }
 
     return current;
 }
 
+// 复制链表
 luo_dlist *
 luoDListDup(luo_dlist *dlist) {
     luo_dlist      *copy;
     luo_dlist_iter *iter;
     luo_dlist_node *node;
 
+    // 创建一个空链表
     copy = luoDListCreate();
 
+    // 创建失败直接返回NULL
     if (copy == NULL) {
         return NULL;
     }
 
+    // 复制原链表的复制/内存释放/匹配函数指针
     copy->dup   = dlist->dup;
     copy->free  = dlist->free;
     copy->match = dlist->match;
 
+    // 获取链表迭代器
     iter = luoDListGetIterator(dlist, LUO_DLIST_START_HEAD);
 
+    // 根据迭代器遍历节点
     while ((node = luoDListNext(iter)) != NULL) {
         void *value;
 
         if (copy->dup) {
+            // 如果定义了复制方法,使用复制方法复制节点值
             value = copy->dup(node->value);
             if (value == NULL) {
+                // 复制失败,释放资源并返回NULL
                 luoDListRelease(copy);
                 luoDListReleaseIterator(iter);
                 return NULL;
             }
         } else {
+            // 未定义复制方法,直接复制节点指针
             value = node->value;
         }
 
+        // 从链表尾部添加节点
         if (luoDListAddNodeTail(copy, value) == NULL) {
+            // 添加失败,释放资源并返回NULL
             luoDListRelease(copy);
             luoDListReleaseIterator(iter);
             return NULL;
         }
     }
 
+    // 释放迭代器
     luoDListReleaseIterator(iter);
 
     return copy;
 }
 
+// 根据key搜索链表节点
 luo_dlist_node *
 luoDListSearchKey(luo_dlist *dlist, void *key) {
     luo_dlist_iter *iter;
     luo_dlist_node *node;
 
+    // 获取链表迭代器,并设置从链表头开始遍历
     iter = luoDListGetIterator(dlist, LUO_DLIST_START_HEAD);
 
     while ((node = luoDListNext(iter)) != NULL) {
         if (dlist->match) {
+            // 如果定义了匹配方法,使用匹配方法查找key
             if (dlist->match(node->value, key)) {
+                // 释放迭代器并返回节点
                 luoDListReleaseIterator(iter);
                 return node;
             }
         } else {
+            // 如果未定义匹配方法,直接根据key比较
             if (key == node->value) {
+                // 释放迭代器并返回节点
                 luoDListReleaseIterator(iter);
                 return node;
             }
         }
     }
 
+    // 未匹配到节点,释放迭代器并返回NULL
     luoDListReleaseIterator(iter);
     return NULL;
 }
 
+// 根据链表下标获取节点
 luo_dlist_node *
 luoDListIndex(luo_dlist *dlist, int index) {
     luo_dlist_node *node;
 
     if (index < 0) {
+        // 如果下标为负数,则从后往前遍历
         index = (-index) - 1;
         node  = dlist->tail;
         while (index-- && node) {
             node = node->prev;
         }
     } else {
+        // 如果下标为正数,则从前往后遍历
         node = dlist->head;
         while (index-- && node) {
             node = node->next;
         }
     }
 
+    // 返回下标对应的节点
     return node;
 }
 
+// 重置迭代器方向,从链表头开始
 void
 luoDListRewind(luo_dlist *dlist) {
+    // 设置迭代器下个处理节点为链表头
     dlist->iter.next      = dlist->head;
+    // 设置迭代器方向从链表头开始
     dlist->iter.direction = LUO_DLIST_START_HEAD;
 }
 
+// 重置迭代器方向,从链表尾开始
 void
 luoDListRewindTail(luo_dlist *dlist) {
+    // 设置迭代器下个处理节点为链表尾
     dlist->iter.next      = dlist->tail;
+    // 设置迭代器方向从链表尾开始
     dlist->iter.direction = LUO_DLIST_START_TAIL;
 }
 
