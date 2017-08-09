6fbef27eb7aec7c49bb024ecd4a19a30f7cf8625
wuleying <5590548@qq.com>
2017-08-09 15:47:31 +0800

    update

--- luodb_v1/luodb/memory/mem.c
+++ luodb_v1/luodb/memory/mem.c
@@ -10,7 +10,7 @@
 
 #include <luodb/memory/mem.h>
 
-// 已分配内存
+// 已使用内存
 static size_t _used_memory = 0;
 
 // 分配一块指定大小的内存区域
@@ -29,50 +29,67 @@ luoMalloc(size_t size) {
     // 更新已使用内存
     _used_memory += size + sizeof(size_t);
 
-    // 返回指向该区域头部的指针
+    // 返回指向该区域头部指针
     return (char *) ptr + sizeof(size_t);
 }
 
 // 对luoMalloc申请的内存进行动态大小调整
 void *
 luoRealloc(void *ptr, size_t size) {
+    // 原始内存头部指针
     void   *real_ptr;
+    // 重新分配内存头部指针
     void   *new_ptr;
+    // 原始内存空间大小
     size_t old_size;
 
+    // 原始内存头部指针为空,直接分配指定大小内存
     if (ptr == NULL) {
         return luoMalloc(size);
     }
 
+    // 获取原始内存头部指针
     real_ptr = (char *) ptr - sizeof(size_t);
+    // 获取原始内存空间大小
     old_size = *((size_t *) real_ptr);
+    // 重新分配内存
     new_ptr  = realloc(real_ptr, size + sizeof(size_t));
 
+    // 分配内存失败,直接返回空
     if (!new_ptr) {
         return NULL;
     }
 
     *((size_t *) new_ptr) = size;
+
+    // 更新内存使用量
     _used_memory -= old_size;
     _used_memory += size;
 
+    // 返回指向重分配内存区域头部指针
     return (char *) new_ptr + sizeof(size_t);
 }
 
 // 释放ptr指向的存储空间
 void
 luoFree(void *ptr) {
+    // 内存区域头部指针
     void   *real_ptr;
+    // 内存空间大小
     size_t old_size;
 
+    // 内存区域头部指针为空,直接返回
     if (ptr == NULL) {
         return;
     }
 
     real_ptr = (char *) ptr - sizeof(size_t);
     old_size = *((size_t *) real_ptr);
+
+    // 更新内存使用量
     _used_memory -= old_size + sizeof(size_t);
 
+    // 释放内存
     free(real_ptr);
 }
 
