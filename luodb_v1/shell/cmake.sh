51bc0193670732f23156bad63ebae52052e60a38
wuleying <5590548@qq.com>
2017-08-15 10:24:32 +0800

    modify cmake.sh

--- luodb_v1/shell/cmake.sh
+++ luodb_v1/shell/cmake.sh
@@ -14,24 +14,23 @@ ENV_BUILD_DIR=""
 ENV_BIN_DIR=""
 
 # 编译参数
-ENV_BUILD_MODE="server"
+PARAM_BUILD_MODE="server"
 
 # 初始化
 init(){
     # 编译参数
     if [ -n "$1" ]; then
-        ENV_BUILD_MODE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+        PARAM_BUILD_MODE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
     fi
-
-    _print "ENV_BUILD_MODE:   $ENV_BUILD_MODE"
 
     ENV_ROOT_DIR=$(cd "$(dirname "$1")" || exit; pwd)
     ENV_BUILD_DIR="$ENV_ROOT_DIR"/build
     ENV_BIN_DIR="$ENV_ROOT_DIR"/bin
 
-     _print "ENV_ROOT_DIR:     $ENV_ROOT_DIR"
-     _print "ENV_BUILD_DIR:    $ENV_BUILD_DIR"
-     _print "ENV_BIN_DIR:      $ENV_BIN_DIR"
+    _print "PARAM_BUILD_MODE:   $PARAM_BUILD_MODE"
+    _print "ENV_ROOT_DIR:       $ENV_ROOT_DIR"
+    _print "ENV_BUILD_DIR:      $ENV_BUILD_DIR"
+    _print "ENV_BIN_DIR:        $ENV_BIN_DIR"
 }
 
 # 清理工作
@@ -42,8 +41,8 @@ clean(){
         rm -rf "$ENV_BUILD_DIR"
     fi
 
-    if [[ -f "$ENV_BIN_DIR/$PROJECT_NAME-$ENV_BUILD_MODE" ]]; then
-        rm "$ENV_BIN_DIR/$PROJECT_NAME-$ENV_BUILD_MODE"
+    if [[ -f "$ENV_BIN_DIR/$PROJECT_NAME-$PARAM_BUILD_MODE" ]]; then
+        rm "$ENV_BIN_DIR/$PROJECT_NAME-$PARAM_BUILD_MODE"
     fi
 
     _print "end..."
@@ -69,8 +68,8 @@ build() {
     cd "$ENV_BUILD_DIR" || exit
     _print "cd $ENV_BUILD_DIR"
 
-    cmake -D BUILD_MODE="$ENV_BUILD_MODE" ..
-    _print "cmake -D BUILD_MODE=${ENV_BUILD_MODE} .."
+    cmake -D BUILD_MODE="$PARAM_BUILD_MODE" ..
+    _print "cmake -D BUILD_MODE=${PARAM_BUILD_MODE} .."
 
     make
     _print "make"
@@ -88,6 +87,7 @@ build() {
     _print "done..."
 }
 
+# 控制台输出
 _print() {
     printf "[%-5s] %s\n" "${FUNCNAME[1]}" "$1"
 }
